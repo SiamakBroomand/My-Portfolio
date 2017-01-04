4a51e3812f20201afb2e4a0d946698469e93122d
Siamak Broomand <broomand.siamak85@gmail.com>
2017-01-03 20:49:27 -0800

    added portfolio.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+var articles = [];
+
+fucntion Article (info){
+  this.title = title;
+  this.category = category;
+  this.author = author;
+  this.authorUrl = authorUrl;
+  this.publishedOn = publishedOn;
+  this.body = body;
+}
