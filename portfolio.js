b9020d04f5dfe34e6dd65952ca87df391b7a4ff2
Siamak Broomand <broomand.siamak85@gmail.com>
2017-01-05 22:38:25 -0800

    added some parts like lab to my files, added image and functionality

--- portfolio.js
+++ portfolio.js
@@ -6,7 +6,7 @@ var sourceData = [
     imgsrc:      '',
     author:    'Siamak Broomand',
     publishedOn: '',
-    mission:        '<p>Mission: <br>To make a website in which <strong> kids </strong> can either type or ask their parents to type the story they tell based on the photo shown.  This is basically designed to <em> avoid repetition </em> of bedtime stories and most important of all to make kids <strong> creative </strong>.  </p>',
+    mission:        '<p>Mission: <br>To make a website in which <strong> kids </strong> can either type or ask their parents to type the story they tell based on the photo shown.  This website is basically designed to <em> avoid repetition </em> of bedtime stories and most important of all to make kids <strong> creative </strong>.  </p>',
     methodsUsed: '<p>used methods</p>',
     githubUrl:   'https://amphib24.github.io/storybook-project/story.html',
   },
